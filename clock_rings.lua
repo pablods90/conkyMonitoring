a1ee46732c82ee9fd36b34735e2e0479e3f2ba69
Pablo <7073164+pablods90@users.noreply.github.com>
2021-02-28 00:16:33 +0000

    Updating to work with Ubuntu 20.04.

--- clock_rings.lua
+++ clock_rings.lua
@@ -8,7 +8,7 @@ IMPORTANT: if you are using the 'cpu' function, it will cause a segmentation fau
 To call this script in Conky, use the following (assuming that you save this script to ~/scripts/rings.lua):
     lua_load ~/scripts/clock_rings.lua
     lua_draw_hook_pre clock_rings
-    
+
 Changelog:
 + v1.0 -- Original release (30.09.2009)
    v1.1p -- Jpope edit londonali1010 (05.10.2009)
@@ -30,7 +30,7 @@ settings_table = {
         -- "bg_alpha" is the alpha value of the base ring.
         bg_alpha=0.1,
         -- "fg_colour" is the colour of the indicator part of the ring.
-        fg_colour=0x339900,
+        fg_colour=0xdb3a24,
         -- "fg_alpha" is the alpha value of the indicator part of the ring.
         fg_alpha=0.2,
         -- "x" and "y" are the x and y coordinates of the centre of the ring, relative to the top left corner of the Conky window.
@@ -50,7 +50,7 @@ settings_table = {
         max=60,
         bg_colour=0xffffff,
         bg_alpha=0.1,
-        fg_colour=0x339900,
+        fg_colour=0xdb3a24,
         fg_alpha=0.4,
         x=140, y=120,
         radius=1.55*56,
@@ -64,7 +64,7 @@ settings_table = {
         max=60,
         bg_colour=0xffffff,
         bg_alpha=0.1,
-        fg_colour=0x339900,
+        fg_colour=0xdb3a24,
         fg_alpha=0.6,
         x=140, y=120,
         radius=1.55*62,
@@ -78,7 +78,7 @@ settings_table = {
         max=31,
         bg_colour=0xffffff,
         bg_alpha=0.1,
-        fg_colour=0x339900,
+        fg_colour=0xdb3a24,
         fg_alpha=0.8,
         x=140, y=120,
         radius=1.55*70,
@@ -92,7 +92,7 @@ settings_table = {
         max=12,
         bg_colour=0xffffff,
         bg_alpha=0.1,
-        fg_colour=0x339900,
+        fg_colour=0xdb3a24,
         fg_alpha=1,
         x=140, y=120,
         radius=1.55*76,
@@ -106,7 +106,7 @@ settings_table = {
         max=140,
         bg_colour=0xffffff,
         bg_alpha=0.2,
-        fg_colour=0x339900,
+        fg_colour=0xdb3a24,
         fg_alpha=0.8,
         x=50, y=300,
         radius=1.55*25,
@@ -120,7 +120,7 @@ settings_table = {
         max=140,
         bg_colour=0xffffff,
         bg_alpha=0.2,
-        fg_colour=0x339900,
+        fg_colour=0xdb3a24,
         fg_alpha=0.8,
         x=75, y=350,
         radius=1.55*25,
@@ -134,7 +134,7 @@ settings_table = {
         max=140,
         bg_colour=0xffffff,
         bg_alpha=0.2,
-        fg_colour=0x339900,
+        fg_colour=0xdb3a24,
         fg_alpha=0.8,
         x=140, y=400,
         radius=1.55*25,
@@ -148,7 +148,7 @@ settings_table = {
         max=140,
         bg_colour=0xffffff,
         bg_alpha=0.2,
-        fg_colour=0x339900,
+        fg_colour=0xdb3a24,
         fg_alpha=0.8,
         x=125, y=450,
         radius=1.55*25,
@@ -162,7 +162,7 @@ settings_table = {
         max=140,
         bg_colour=0xffffff,
         bg_alpha=0.2,
-        fg_colour=0x339900,
+        fg_colour=0xdb3a24,
         fg_alpha=0.8,
         x=150, y=500,
         radius=1.55*25,
@@ -206,7 +206,7 @@ end
 
 function draw_ring(cr,t,pt)
     local w,h=conky_window.width,conky_window.height
-    
+
     local xc,yc,ring_r,ring_w,sa,ea=pt['x'],pt['y'],pt['radius'],pt['thickness'],pt['start_angle'],pt['end_angle']
     local bgc, bga, fgc, fga=pt['bg_colour'], pt['bg_alpha'], pt['fg_colour'], pt['fg_alpha']
 
@@ -220,56 +220,56 @@ function draw_ring(cr,t,pt)
     cairo_set_source_rgba(cr,rgb_to_r_g_b(bgc,bga))
     cairo_set_line_width(cr,ring_w)
     cairo_stroke(cr)
-    
+
     -- Draw indicator ring
 
     cairo_arc(cr,xc,yc,ring_r,angle_0,angle_0+t_arc)
     cairo_set_source_rgba(cr,rgb_to_r_g_b(fgc,fga))
-    cairo_stroke(cr)        
+    cairo_stroke(cr)
 end
 
 function draw_clock_hands(cr,xc,yc)
     local secs,mins,hours,secs_arc,mins_arc,hours_arc
     local xh,yh,xm,ym,xs,ys
-    
-    secs=os.date("%S")    
+
+    secs=os.date("%S")
     mins=os.date("%M")
     hours=os.date("%I")
-        
+
     secs_arc=(2*math.pi/60)*secs
     mins_arc=(2*math.pi/60)*mins+secs_arc/60
     hours_arc=(2*math.pi/12)*hours+mins_arc/12
-        
+
     -- Draw hour hand
-    
+
     xh=xc+0.7*clock_r*math.sin(hours_arc)
     yh=yc-0.7*clock_r*math.cos(hours_arc)
     cairo_move_to(cr,xc,yc)
     cairo_line_to(cr,xh,yh)
-    
+
     cairo_set_line_cap(cr,CAIRO_LINE_CAP_ROUND)
     cairo_set_line_width(cr,5)
     cairo_set_source_rgba(cr,1.0,1.0,1.0,1.0)
     cairo_stroke(cr)
-    
+
     -- Draw minute hand
-    
+
     xm=xc+clock_r*math.sin(mins_arc)
     ym=yc-clock_r*math.cos(mins_arc)
     cairo_move_to(cr,xc,yc)
     cairo_line_to(cr,xm,ym)
-    
+
     cairo_set_line_width(cr,3)
     cairo_stroke(cr)
-    
+
     -- Draw seconds hand
-    
+
     if show_seconds then
         xs=xc+clock_r*math.sin(secs_arc)
         ys=yc-clock_r*math.cos(secs_arc)
         cairo_move_to(cr,xc,yc)
         cairo_line_to(cr,xs,ys)
-    
+
         cairo_set_line_width(cr,1)
         cairo_stroke(cr)
     end
@@ -279,32 +279,32 @@ function conky_clock_rings()
     local function setup_rings(cr,pt)
         local str=''
         local value=0
-        
+
         str=string.format('${%s %s}',pt['name'],pt['arg'])
         str=conky_parse(str)
-        
+
         value=tonumber(str)
         if ( value == nil ) then value = 0 end
         pct=value/pt['max']
-        
+
         draw_ring(cr,pct,pt)
     end
-    
+
     -- Check that Conky has been running for at least 5s
 
     if conky_window==nil then return end
     local cs=cairo_xlib_surface_create(conky_window.display,conky_window.drawable,conky_window.visual, conky_window.width,conky_window.height)
-    
-    local cr=cairo_create(cs)    
-    
+
+    local cr=cairo_create(cs)
+
     local updates=conky_parse('${updates}')
     update_num=tonumber(updates)
-    
+
     if update_num>5 then
         for i in pairs(settings_table) do
             setup_rings(cr,settings_table[i])
         end
     end
-    
+
     draw_clock_hands(cr,clock_x,clock_y)
 end
